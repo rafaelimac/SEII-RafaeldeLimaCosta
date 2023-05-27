d737b39dbd9757e2c218452f69291dae4c144af6
amandabtrochez <127358009+rafaelimac@users.noreply.github.com>
2023-05-27 14:59:12 -0300

    Create file7.py

---
+++ Semana02/EXERCICIO04/file7.py
@@ -0,0 +1,28 @@
+#work with loop
+nums = [1,2,3,4,5]
+for num in nums:
+    print(num)
+
+for num in nums:
+    if num ==3:
+        print('Found!')
+        break
+    print(num)
+
+for num in nums:
+    if num ==3:
+        print('Found!')
+        continue
+    print(num)
+
+for num in nums:
+    for latter in 'abc' :
+        print(num,latter)
+
+for i in range(1,11):
+    print(i)
+
+x = 0
+while x < 10:
+    print(x)
+    x+=1 
