46db9df9252d5a87e18a5a899d46289f2159082f
amandabtrochez <127358009+rafaelimac@users.noreply.github.com>
2023-05-27 15:04:55 -0300

    Create file09.py

---
+++ semana04/exercicio02/file09.py
@@ -0,0 +1,14 @@
+class Student:
+	def __init__(self, name, age, grade):
+		self.name = name
+		self.age = age
+		self.grade = grade # 0 - 100
+
+	def get_grade(self):
+		return self.grade
+
+class Course:
+	def __init__(self, name, max_students):
+		self.name = name
+		self.max_students = max_students
+		self.students = []
