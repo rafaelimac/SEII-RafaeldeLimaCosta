fe964ab829a4d654bbf6610ec041f8db3bc7506f
amandabtrochez <127358009+rafaelimac@users.noreply.github.com>
2023-05-27 14:59:01 -0300

    Create file3.py

---
+++ Semana02/EXERCICIO04/file3.py
@@ -0,0 +1,23 @@
+#work whith number
+num = 3
+print(type(num))
+num = -3.14
+print(type(num))
+
+print('soma: ',3+2 , ' sub:',3-2 ,' mult: ', 3*3)
+print('div: {} divsuc: {}'.format(3/2,3//2))
+print(f'exp: {3**2} resto: {3%2}')
+
+print(abs(num))
+print(round(num))
+print(round(num,1))
+num2 = 2
+num3 = 3
+
+print (num3 == num2,num3 != num2,num3 > num2,num3 < num2,num3 >= num2,num3 <= num2)
+
+
+num4 = '100'
+num5 = '200'
+print (num4 + num5)
+print (int(num4) + int(num5))
