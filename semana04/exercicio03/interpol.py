14b8a492abe6dc8aea589ba840f32a4e71535329
amandabtrochez <127358009+rafaelimac@users.noreply.github.com>
2023-05-27 15:05:18 -0300

    Create interpol.py

---
+++ semana04/exercicio03/interpol.py
@@ -0,0 +1,12 @@
+x = np.linspace(0, 10, 10)
+y = x**2 * np.sin(x)
+plt.scatter(x,y)
+
+from scipy.interpolate import interp1d
+
+f = interp1d(x, y, kind='cubic')
+x_dense = np.linspace(0, 10, 100)
+y_dense = f(x_dense)
+
+plt.plot(x_dense, y_dense)
+plt.scatter(x, y)
